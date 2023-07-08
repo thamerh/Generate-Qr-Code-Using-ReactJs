055c5b99796e045f8dc94c88b847a33f7c057814
thamerh <hamdithamer99@gmail.com>
2023-07-08 19:44:42 +0200

    feat: create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+   return (
+      <footer className="attribution">
+         <p>developed by <a href="https://github.com/thamerh" target="_blank" rel="noreferrer">Thamer Hamdi</a></p>
+      </footer>
+   );
+}
+
+export default Footer;
