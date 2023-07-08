03c59b59fd8bab3fa7a3305adbe3cec807b3d8f5
thamerh <hamdithamer99@gmail.com>
2023-07-08 19:46:36 +0200

    feat: create generate qrCode component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import html2canvas from "html2canvas";
+import { QRCodeCanvas } from "qrcode.react";
+import React, { useState } from "react";
+import Footer from './components/Footer';
+import { AiFillCopy, AiOutlineDownload } from "react-icons/ai";
 
 function App() {
+  const [qr, setqr] = useState("");
+  const [url, seturl] = useState("");
+  const QrCodeDownload = async () => {
+    const canvas = await (
+      await html2canvas(document.getElementById("canvas"))
+    ).toDataURL();
+
+    if (canvas) {
+      const a = document.createElement("a");
+      a.download = "QrCode.png";
+      a.href = canvas;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  };
+
+  const QrCodeCopy = async () => {
+	const canvas = await (
+		await html2canvas(document.getElementById("canvas"))
+	  ).toDataURL();
+	  if (canvas) {
+		  setqr(canvas);
+		  navigator.clipboard.writeText(qr);
+		}
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+	<>
+	<main>
+	<div>
+      <h1 style={{ width: "100%" }}>Generate your Qr Code</h1>
+      <div class="mb-4">
+        <label>Enter your Data</label>
+        <input
+          placeholder="Add Your Data for Qr Code generation"
+          required
+          style={{ marginTop: 10 }}
+          type="text"
+          onChange={(e) => seturl(e.target.value)}
+        />
+		 <button onClick={() => QrCodeDownload()} class="DownloadButton">
+          <AiOutlineDownload />
+          Download
+        </button>
+        <button onClick={() => QrCodeCopy()} class="CoppyButton">
+          <AiFillCopy />
+          Copy
+        </button>
+      </div>
+	  </div>
+      <article className="card">
+        <div id="canvas" className="qr-box">
+          <QRCodeCanvas
+            value={url ? url : "https://github.com/thamerh"}
+            size={300}
+            bgColor={"#ffffff"}
+            fgColor={"#0a75ad"}
+            level={"H"}
+            includeMargin={false}
+            imageSettings={{
+              src: "/thamer.jpg",
+              x: undefined,
+              y: undefined,
+              height: 60,
+              width: 60,
+              excavate: true,
+            }}
+          />
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter your Data and create your custom Qr Code in a few seconds with a
+          few clicks.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </article>
+    
+  
+	</main>
+	<Footer/>
+</>
   );
 }
 
